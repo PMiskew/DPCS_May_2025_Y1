65b3df5c399e6a0d202a5656dca3a96523ccee3e
pmiskew <pmiskew@10.100.2.119>
2023-11-20 13:49:54 -0500

    update

--- src/Tool1.java
+++ src/Tool1.java
@@ -12,8 +12,13 @@ public class Tool1 {
 		int[] nums1 = {1,2,5,3,5}; // sum = 16 
 		int[] nums2 ={1,1,1,1,1}; //sum = 5
 		int[] nums3 = {};// sum = 0;
-		int[] num4 = {99}; //sum = 99
+		int[] nums4 = {99}; //sum = 99
 		
+		System.out.println(findSum(nums1));
+		System.out.println(findSum(nums2));
+		System.out.println(findSum(nums3));
+		System.out.println(findSum(nums4));
+	
 	} //end main method
 	
 	
@@ -46,6 +51,78 @@ public class Tool1 {
 		return sum;
 				
 	}//end findSum
+	
+	//Homework:  I want you to start creating your modsum methods
+	/*
+	 * A modSum method is one that find the sum of an array of integers
+	 * (or some other structur), but modifies the approach
+	 * 
+	 * public static int findSumOfEven(int[] a) {
+	 * 
+	 * }
+	 * 
+	 */
  
+	/**
+	 * 
+	 * @param a
+	 * @return
+	 */
+	public static int findSumOfEven(int[] a) {
+		 
+		int sum = 0; 
+		
+		for (int i = 0; i < a.length; i = i + 1) {
+			
+			if (a[i] % 2 == 0) {
+				sum = sum + a[i];
+			}
+		}
+		
+		return sum;
+	}
+	
+	/**
+	 * 
+	 * @param a
+	 * @return
+	 */
+	public static int findSumOfOdd(int [] a) {
+		
+		int sum = 0; 
+		
+		for (int i = 0; i < a.length; i = i + 1) {
+			
+			if (a[i] % 2 != 0) {
+				sum = sum + a[i];
+			}
+		}
+		
+		return sum;
+	}
+	
+	/**
+	 * This returns the sum of integers starting at index 0 add 
+	 * num integers.  For example if a = {1,2,3,4,5} and num = 2
+	 * then the method would add the first 2 elements. 
+	 * @param a
+	 * @param num
+	 * @return
+	 */
+	public static int findSumOfNValues(int[] a, int num) {
+		
+		int sum = 0; 
+		
+		for (int i = 0; i < a.length; i = i + 1) {
+			
+			if (a[i] % 2 != 0) {
+				sum = sum + a[i];
+			}
+		}
+		
+		return sum;
+		
+		
+	}
 
 } //end of the class - nothing goes beyond this braket
---
+++ src/Tool2.java
@@ -0,0 +1,42 @@
+import java.util.Queue;
+import java.util.Stack;
+
+public class Tool2 {
+
+	public static void main(String[] args) {
+		
+		String s = null;
+		reverseWordA(s);
+		
+	}
+	
+	/**
+	 * This method takes a string parameter and returns the 
+	 * word in reverse. For example if word = "cat" the method
+	 * would return "tac"
+	 * 
+	 * The method should return a null reference if the String passed
+	 * is null. 
+	 * @param word
+	 * @return
+	 */
+	public static String reverseWordA(String word) {
+		
+		System.out.println(word);
+		return null;
+	}
+	
+	public static String[] reverseWordB(String[] words) {
+		
+		return null;
+	}
+	
+	public static Stack<String> reverseWordC(String[] words) {
+		return null;
+	}
+	
+	public static Queue<String> reverseWordD(Queue<String> words) {
+		return null;
+	}
+
+}
